46ca8b65283dad37a4f392fb7b24df5044c2379b
pjm927 <82696634+pjm927@users.noreply.github.com>
2026-08-20 01:27:36 +0900

    Update bubble_sort.py

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,13 +1,12 @@
 def bubble_sort(arr):
     n = len(arr)
 
+    # 한 번의 반복마다 가장 큰 값을 배열의 뒤쪽으로 이동
     for i in range(n - 1):
         for j in range(n - 1 - i):
+
+            # 인접한 두 원소의 순서가 잘못되어 있으면 교환
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
-
-
-numbers = [5, 3, 8, 4, 2]
-print(bubble_sort(numbers))
